51ff8acb4a04cf2e5f43a5b5426d80b188f326b5
Joe Polo <joe.polo@telus.com>
2022-12-14 17:36:10 -0800

    build node postgresql queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`SELECT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts on cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%' 
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;`)
+
+.then(res =>  {
+  res.rows.forEach(row => {
+    console.log(`The teacher ${row.teacher} assisted the cohort ${row.cohort}`);
+  })
+}).catch(err => console.error('query error', err.stack));
